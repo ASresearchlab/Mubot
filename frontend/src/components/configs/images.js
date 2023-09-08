a284ec26b474d2007242a70bebda9f18be1277a8
ASresearchlab <106665055+ASresearchlab@users.noreply.github.com>
2023-09-08 15:41:15 +0300

    Add files via upload

---
+++ frontend/src/components/configs/images.js
@@ -0,0 +1,8 @@
+export const Images = {
+    logo: require('../assets/images/logo.png'),
+    mubot: require('../assets/images/mubot-modified.png'),
+    verify: require('../assets/images/success.png'),
+    logo2: require('../assets/images/logo2.png'),
+  };
+  
+  
