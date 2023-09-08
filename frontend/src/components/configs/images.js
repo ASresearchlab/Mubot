90336cbc84d0401b63f2751554e807d2f67a0833
ASresearchlab <106665055+ASresearchlab@users.noreply.github.com>
2023-09-08 12:45:36 +0300

    Update images.js

--- frontend/src/components/configs/images.js
+++ frontend/src/components/configs/images.js
@@ -1,7 +1,7 @@
 export const Images = {
     logo: require('../assets/images/logo.png'),
     mubot: require('../assets/images/mubot-modified.png'),
-    veriy: require('../assets/images/success.png'),
+    verify: require('../assets/images/success.png'),
     logo2: require('../assets/images/logo2.png'),
   };
   
