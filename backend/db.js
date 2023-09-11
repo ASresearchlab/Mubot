3f6026a84793ec0bd919f0c6aba7931e19482764
ASresearchlab <106665055+ASresearchlab@users.noreply.github.com>
2023-09-11 12:24:17 +0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -7,7 +7,7 @@ module.exports = () => {
 	};
 	try {
 		mongoose.set('strictQuery', false);
-		mongoose.connect('mongodb+srv://mubot:1531998aA%40@react-dashboard.ofgcwmn.mongodb.net/authInMern', connectionParams);
+		mongoose.connect(process.production.DB, connectionParams);
 		console.log("Connected to database successfully");
 	} catch (error) {
 		console.log(error);
