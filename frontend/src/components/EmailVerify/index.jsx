2dafeb04bdafc01b6195628c444607e099ebabcc
ASresearchlab <106665055+ASresearchlab@users.noreply.github.com>
2023-09-11 13:53:03 +0300

    Update index.jsx

--- frontend/src/components/EmailVerify/index.jsx
+++ frontend/src/components/EmailVerify/index.jsx
@@ -12,7 +12,7 @@ const EmailVerify = () => {
 	useEffect(() => {
 		const verifyEmailUrl = async () => {
 			try {
-				const url = `https://museumbot-frontend.onrender.com/api/users/${param.id}/verify/${param.token}`;
+				const url = `https://museumbot-backend.onrender.com/api/users/${param.id}/verify/${param.token}`;
 				const { data } = await axios.get(url);
 				console.log(data);
 				setValidUrl(true);
